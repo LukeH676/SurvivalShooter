5ff546d95e4e783975aa7905f30d33bffd4be3ec
Luke H <LukeH676@users.noreply.github.com>
2017-06-01 19:54:12 -0400

    Spawn points
    Spawn points added - enemies spawn at three different locations

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -10,19 +10,19 @@ public class EnemyManager : MonoBehaviour
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);// don't need a time for something thats goign to repeat - calls spawn function - Amount of time to wait before spawn - amount fo time to wait after spawn
     }
 
 
     void Spawn ()
     {
-        if(playerHealth.currentHealth <= 0f)
+        if(playerHealth.currentHealth <= 0f)//if player is dead don't spawn - if allive spawn
         {
             return;
         }
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length); // random number up to the maximum length
 
-        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation); // createa an instance of something - thing to spawn - where to spawn - what rotation it hsould have
     }
 }
